9937fe1c5de194bc38cb3e4a5a63af0f9feaf0ba
Khaled Essam <khaled.wt@hotmail.com>
2020-10-19 23:42:58 +0200

    More tests

--- test/ga_clj/genetic_test.clj
+++ test/ga_clj/genetic_test.clj
@@ -4,8 +4,20 @@
 
 (t/deftest swap-test
   (t/testing "swap"
-    (t/is (= (g/swap [1 2 3 4] 0 1) [2 1 3 4]))))
+    (t/is (= [2 1 3 4] (g/swap [1 2 3 4] 0 1)))))
 
 (t/deftest find-first-test
   (t/testing "find-first"
     (t/is (= 6 (g/find-first #(> % 5) (range 10))))))
+
+(t/deftest cumsum-test
+  (t/testing "cumsum"
+    (t/is (= [1 2 3 4] (g/cumsum [1 1 1 1])))))
+
+(t/deftest sum-test
+  (t/testing "sum"
+    (t/is (= 10 (g/sum [1 2 3 4])))))
+
+(t/deftest div-test
+  (t/testing "div"
+    (t/is (= [1 1/2 2 3/2] (g/div [2 1 4 3] 2)))))
